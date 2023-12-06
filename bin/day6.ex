ed62c66adc755be41780724dc82cd8fb1a774edb
Paul C. Mantz <pcmantz@gmail.com>
2023-12-06 00:43:51 -0600

    day6 code cleanup

--- bin/day6.ex
+++ bin/day6.ex
@@ -10,30 +10,29 @@ defmodule Day6 do
 
     def record_beating_score(races) do
       races
-      |> Enum.map(&( winning_times(&1) ))
-      |> Enum.map(&( Enum.count(&1) ))
-      |> Enum.product
+      |> Enum.map(&winning_times(&1))
+      |> Enum.map(&Enum.count(&1))
+      |> Enum.product()
     end
 
     def winning_times(race) do
       %{time: time, record: record} = race
 
-      winning_holds = (0..time)
-      |> Enum.map(&( Race.distance_for_hold_and_time(&1, time) ))
-      |> Enum.filter(&( &1 > record ))
+      winning_holds =
+        0..time
+        |> Enum.map(&Race.distance_for_hold_and_time(&1, time))
+        |> Enum.filter(&(&1 > record))
     end
-
 
     def long_race_winning_times do
-      time = 60808676
-      record = 601116315591300
+      time = 60_808_676
+      record = 601_116_315_591_300
 
-      (14..(time-14))
-      |> Stream.map(&( Race.distance_for_hold_and_time(&1, time) ))
-      |> Stream.filter(&( &1 > record ))
-      |> Enum.count
+      14..(time - 14)
+      |> Stream.map(&Race.distance_for_hold_and_time(&1, time))
+      |> Stream.filter(&(&1 > record))
+      |> Enum.count()
     end
-
 
     def distance_for_hold_and_time(hold, time) do
       run_time = time - hold
@@ -42,36 +41,36 @@ defmodule Day6 do
     end
   end
 
-
   defmodule Parser do
     @moduledoc """
     Parser for Day 6. This should read a file and turn it into the relevant structs. defined above.
     """
 
     def read_file(filename) do
-      lines = File.read(filename)|> elem(1) |> String.split("\n")
+      lines = File.read(filename) |> elem(1) |> String.split("\n")
       [time_line, distance_line, _] = lines
 
-      [ _, times_string] = Regex.run(~r/^Time:\s+(.*)$/, time_line)
-      [ _, distances_string] = Regex.run(~r/^Distance:\s+(.*)$/, distance_line)
+      [_, times_string] = Regex.run(~r/^Time:\s+(.*)$/, time_line)
+      [_, distances_string] = Regex.run(~r/^Distance:\s+(.*)$/, distance_line)
 
       times = parse_number_string(times_string)
       distances = parse_number_string(distances_string)
       race_pairs = Enum.zip([times, distances])
 
-      Enum.map(race_pairs, &( struct(Race, %{ time: elem(&1, 0), record: elem(&1, 1) })))
+      Enum.map(race_pairs, &struct(Race, %{time: elem(&1, 0), record: elem(&1, 1)}))
     end
 
     def parse_number_string(number_string) do
-      strings = number_string
-      |> String.trim
-      |> String.split(~r/\s+/)
+      strings =
+        number_string
+        |> String.trim()
+        |> String.split(~r/\s+/)
 
-      Enum.map(strings, &( string_to_integer(&1) ))
+      Enum.map(strings, &string_to_integer(&1))
     end
 
     def string_to_integer(str) do
-      str |> String.trim |> Integer.parse |> elem(0)
+      str |> String.trim() |> Integer.parse() |> elem(0)
     end
   end
 end
